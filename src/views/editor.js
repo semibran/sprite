bd4c75ce60ddb7a0c466340a8f74ea18573efa12
Brandon Semilla <semibran@gmail.com>
2021-02-13 16:46:59 -0800

    basic sprite selection

--- src/views/editor.js
+++ src/views/editor.js
@@ -5,7 +5,10 @@ import cache from '../app/cache'
 let onmousedown = null
 let onmousemove = null
 let onmouseup = null
-let pressed = false
+let sprites = null
+let select = null
+let pos = null
+let pan = null
 
 export const startPan = (state, { x, y }) => ({
   ...state,
@@ -18,16 +21,19 @@ export const startPan = (state, { x, y }) => ({
   }
 })
 
-export const updatePan = (state, { x, y }) => ({
-  ...state,
-  editor: {
-    ...state.editor,
-    pos: {
-      x: x - state.editor.pan.x,
-      y: y - state.editor.pan.y
+export const updatePan = (state, { x, y }) => {
+  if (!state.editor.pan) return state
+  return {
+    ...state,
+    editor: {
+      ...state.editor,
+      pos: {
+        x: x - state.editor.pan.x,
+        y: y - state.editor.pan.y
+      }
     }
   }
-})
+}
 
 export const endPan = (state) => ({
   ...state,
@@ -35,25 +41,37 @@ export const endPan = (state) => ({
 })
 
 export default function Editor (state, dispatch) {
+  ;({ sprites }) = state
+  ;({ pos, pan }) = state.editor
   const image = cache.image
-  const { pos, pan } = state.editor
   return m('.editor', { class: pan ? '-pan' : '' }, [
     m.fragment({
       oncreate: (vnode) => {
         vnode.dom.addEventListener('mousedown', (onmousedown = (evt) => {
           dispatch(startPan, { x: evt.pageX, y: evt.pageY })
-          pressed = true
         }))
 
         window.addEventListener('mousemove', (onmousemove = (evt) => {
-          if (pressed) {
+          if (pan) {
             dispatch(updatePan, { x: evt.pageX, y: evt.pageY })
+          } else {
+            const rect = vnode.dom.getBoundingClientRect()
+            const x = evt.pageX - rect.left
+            const y = evt.pageY - rect.top
+            select = sprites.find(sprite => {
+              const [left, top, width, height] = sprite.rect
+              if (x >= left
+              && y >= top
+              && x < left + width
+              && y < top + height) {
+                return sprite
+              }
+            })
           }
         }))
 
         window.addEventListener('mouseup', (onmouseup = () => {
           dispatch(endPan)
-          pressed = false
         }))
       },
       onremove: (vnode) => {
@@ -70,6 +88,16 @@ export default function Editor (state, dispatch) {
 
           const context = canvas.getContext('2d')
           const xoffset = canvas.width / 2 - image.width / 2
+
+          sprites.forEach((sprite) => {
+            if (sprite === select) {
+              context.strokeStyle = '#36d'
+            } else {
+              context.strokeStyle = 'black'
+            }
+            context.strokeRect(...sprite.rect)
+          })
+
           context.drawImage(image, Math.round(xoffset), 0)
         }
       }
