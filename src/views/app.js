2ab819f433205da3a134987a01a6a1f5277dc845
Brandon Semilla <semibran@gmail.com>
2021-02-14 00:02:43 -0800

    spiral layout

--- src/views/app.js
+++ src/views/app.js
@@ -9,11 +9,15 @@ import PropsPanel from './panel-props'
 export default function App (state, dispatch) {
   return m('main.app', [
     Header({ title: state.project.name }),
-    m('.content', [
+    m('.content.-row', [
       SpritesPanel(state, dispatch),
-      Editor(state, dispatch),
-      PropsPanel(state, dispatch)
-    ]),
-    Timeline(state, dispatch)
+      m('.content.-col', [
+        m('.content.-row', [
+          Editor(state, dispatch),
+          PropsPanel(state, dispatch)
+        ]),
+        Timeline(state, dispatch)
+      ])
+    ])
   ])
 }
